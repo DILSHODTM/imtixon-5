fcd536681a0f2b194b7afc0fa2f5e8d73aef677e
Dilshod TURSUNOV <113157296+DILSHODTM@users.noreply.github.com>
2023-01-10 15:52:14 +0500

    Create Footer.jsx

---
+++ src/Components/Footer/Footer.jsx
@@ -0,0 +1,55 @@
+import React from 'react';
+import "./footer.scss";
+const footer = () => {
+
+
+
+
+   return (
+      <>
+      
+      <footer className="footer text p-5 " >
+         
+
+<div className="container-footer " >
+   <ul className='d-flex '><i class="fab fa-github gi"></i>
+   <li><p className=' ul'> © 2023 GitHub, Inc.</p></li>
+      <li><a href="https://docs.github.com/en/site-policy/github-terms/github-terms-of-service" className=' fs'>Terms</a></li>
+      <li><a href="https://docs.github.com/en/github/site-policy/github-privacy-statement" className=' fs'>Privacy</a></li>
+      <li><a href="https://github.com/security" className=' fs'>Security</a></li>
+      <li><a href="https://www.githubstatus.com/" className=' fs'>Status</a></li>
+      <li><a href="https://docs.github.com/" className=' fs'>Docs</a></li>
+   <li><a href="https://support.github.com/?tags=dotcom-footer" className=' fs'>Contact</a></li>
+      <li><a href="https://github.com/pricing" className=' fs'>Pricing</a></li>
+      <li><a href="https://docs.github.com/" className=' fs'>API</a></li>
+      <li><a href="https://services.github.com/" className=' fs'>Training</a></li>
+      <li><a href="https://github.blog/" className=' fs'>Blog</a></li>
+      <li><a href="https://github.com/about" className=' fs'>About</a></li>
+
+   </ul>
+   <ul className='d-flexs '><i class="fab fa-github gi"></i>
+      <li><p className=' ul'> © 2023 GitHub, Inc.</p></li>
+      <li><a href="https://docs.github.com/en/site-policy/github-terms/github-terms-of-service" className=' fs'>Terms</a></li>
+      <li><a href="https://docs.github.com/en/github/site-policy/github-privacy-statement" className=' fs'>Privacy</a></li>
+      <li><a href="https://github.com/security" className=' fs'>Security</a></li>
+      <li><a href="https://www.githubstatus.com/" className=' fs'>Status</a></li>
+      
+   </ul>
+   <ul className='d-flexs'>
+      <li><a href="https://docs.github.com/" className=' fs'>Docs</a></li>
+   <li><a href="https://support.github.com/?tags=dotcom-footer" className=' fs'>Contact</a></li>
+      <li><a href="https://github.com/pricing" className=' fs'>Pricing</a></li>
+      <li><a href="https://docs.github.com/" className=' fs'>API</a></li>
+      <li><a href="https://services.github.com/" className=' fs'>Training</a></li>
+      <li><a href="https://github.blog/" className=' fs'>Blog</a></li>
+      <li><a href="https://github.com/about" className=' fs'>About</a></li>
+   </ul>
+</div>
+
+      </footer>
+      
+      </>
+   );
+};
+
+export default footer;
