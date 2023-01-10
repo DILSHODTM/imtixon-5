520104b31af3f3157cf1fe2848e091d4420dc22c
Dilshod TURSUNOV <113157296+DILSHODTM@users.noreply.github.com>
2023-01-10 15:48:25 +0500

    Create Repo.jsx

---
+++ src/pages/Repo/Repo.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import RepoItem from '../../components/RepoItem/RepoItem';
+
+const Repo = () => {
+  return (
+    <>
+      <RepoItem />
+    </>
+  );
+};
+
+export default Repo;
