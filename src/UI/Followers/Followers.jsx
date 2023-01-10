8899f11d877b44c77b6eff774c6fefca398b5ca3
Dilshod TURSUNOV <113157296+DILSHODTM@users.noreply.github.com>
2023-01-10 17:18:09 +0500

    Update Followers.jsx

--- src/UI/Followers/Followers.jsx
+++ src/UI/Followers/Followers.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect, useContext } from "react";
-import { Context } from "../context/Context";
+import { Context } from "../Context/Context";
 import "./Followers.scss";
 
 const Followers = () => {
